647649fbf65ed60ba57b7c2c00db69bafc61a1ba
Prashant Kumar <36854588+samar2326@users.noreply.github.com>
2018-04-22 12:28:21 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,3 +1,5 @@
+"""
+Wap to demonstarte dictionary and related functions"""
 dict={'Roll no':'1613313069','Name':'Prashnat kumar','Course':'Bachelor of Technology','Branch':'I.T'}
 print(dict)
 dict1=dict.copy()
