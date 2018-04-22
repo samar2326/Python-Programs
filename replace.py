4b8a6d1c88653875df907add9da5db5d1b5dda7b
Prashant Kumar <36854588+samar2326@users.noreply.github.com>
2018-04-22 13:17:52 +0530

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,4 +1,4 @@
-""" Program 5b: To get a string from a given string where all occurrences of its first char have
+""" To get a string from a given string where all occurrences of its first char have
 been changed to '$', except the first char itself."""
 def change_char(str):
   str = input("enter a string: ")
