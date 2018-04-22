c30d96ab5552a21a066643983bb714896742154d
Prashant Kumar <36854588+samar2326@users.noreply.github.com>
2018-04-22 12:23:16 +0530

    Update add_ly.py

--- add_ly.py
+++ add_ly.py
@@ -1,5 +1,5 @@
 """
-Program 5a: To add 'ing' at the end of a given string (length should be at least 3). If the
+To add 'ing' at the end of a given string (length should be at least 3). If the
 given string already ends with 'ing' then add 'ly' instead. If the string length of
 the given string is less than 3, leave it unchanged.
 Sample String : 'abc'
